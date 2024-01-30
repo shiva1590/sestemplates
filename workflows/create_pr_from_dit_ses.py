058b1c95ba81bf40107ab07ce9f3047c33d57d75
shiva1590 <shiva_psg@yahoo.co.in>
2024-01-30 17:34:47 +0530

    Update create_pr_from_dit_ses.py

--- workflows/create_pr_from_dit_ses.py
+++ workflows/create_pr_from_dit_ses.py
@@ -6,8 +6,8 @@ import json
 
 # Create Boto3 session using environment variables
 session = boto3.Session(
-          aws_access_key_id="AKIA3KVEPMYVOZQSV6BB",
-          aws_secret_access_key="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",
+          aws_access_key_id="",
+          aws_secret_access_key="",
           region_name="us-east-1"  # Optional
           )
 
